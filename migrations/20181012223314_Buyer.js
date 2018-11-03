52f84a8718fba465ef285bbfef98a41bf12ed7f9
Messi-q <messi.qp711@gmail.com>
2018-11-03 13:11:35 +0800

    solve the bugs of the system

---
+++ migrations/20181012223314_Buyer.js
@@ -0,0 +1,21 @@
+
+exports.up = function (knex, Promise) {
+    return knex.schema.createTable('Buyer', function (table) {
+        table.increments();
+        table.integer('buyerId').unsigned().notNullable();
+        table.integer('fileId').unsigned().notNullable();
+        table.string('fileTitle').notNullable();  //唯一性验证
+        table.string('fileImage').notNullable();
+        table.string('fileDescription').notNullable();
+        table.string('fileReadPrice').notNullable();
+        table.string('fileRightPrice').notNullable();
+        table.string('fileName').notNullable();
+        table.timestamps();
+        table.foreign('buyerId').references('id').inTable('users');
+        table.foreign('fileId').references('id').inTable('files');
+    });
+};
+
+exports.down = function (knex, Promise) {
+    return knex.schema.dropTable('Buyer');
+}
